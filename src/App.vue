031ece84b94cd9e7e36f00e22be6c10e6ec80c86
AmakawaYuki <amakawa.yuki@prevent.co.jp>
2020-05-11 22:29:39 +0900

    add: トップのテンプレートざっくり

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <nav
+      class="navbar is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <div class="navbar-item">
+          <h1 class="title is-4">JS Gugulable</h1>
+        </div>
+        <div class="navbar-item">
+          <p>検索しにくい・ググれないJSの用語がわかるサービス</p>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="columns">
+        <div class="column ">
+          <div class="box">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img
+                    src="https://bulma.io/images/placeholders/96x96.png"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">John Smith</p>
+                <p class="subtitle is-6">@johnsmith</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="box">
+            <div class="card">
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img
+                        src="https://bulma.io/images/placeholders/96x96.png"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <span class="subtitle is-6">@johnsmith</span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="content">
+                <table class="table-card">
+                  <tbody>
+                    <tr>
+                      <td class="has-text-left">
+                        <span class="subtitle is-6">@johnsmithleft</span>
+                      </td>
+                      <td class="has-text-right">
+                        <span class="icon">
+                          <i class="fas fa-home"></i>
+                        </span>
+                        <span class="subtitle is-6">@johnsmith</span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          お問い合わせはこちら
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: "App"
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
+.container {
+  min-height: calc(100vh - 112px);
+}
+.footer {
+  padding: 24px;
 }
 </style>
